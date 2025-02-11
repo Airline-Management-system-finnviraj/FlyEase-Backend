1fcfa0ca28255522061c9d754b9a6d8285039922
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2025-02-11 14:51:52 +0530

    initial commit

---
+++ airlinemanagementsystem/src/test/java/com/FlyEase/airlinemanagementsystem/AirlinemanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlyEase.airlinemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinemanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
